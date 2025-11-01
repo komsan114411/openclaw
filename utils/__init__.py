704def75955bf54f95de312f6c59a48eb0afdf09
komsan114411 <135223364+komsan114411@users.noreply.github.com>
2025-11-02 04:40:37 +0700

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utilities package for LINE OA application
-Contains configuration management and helper functions
-"""
-
-from .config_manager import config_manager
-
-__all__ = ['config_manager']
