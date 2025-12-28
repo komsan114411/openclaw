9d4d7099c3a6b5529c35f06400a5ff51207f6673
limitrack <limitrack@users.noreply.github.com>
2025-12-29 02:55:11 +0700

    fix: add missing next/link import in line-accounts page

--- frontend/src/app/user/line-accounts/page.tsx
+++ frontend/src/app/user/line-accounts/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 import DashboardLayout from '@/components/layout/DashboardLayout';
 import { lineAccountsApi, systemSettingsApi } from '@/lib/api';
 import { LineAccount } from '@/types';
